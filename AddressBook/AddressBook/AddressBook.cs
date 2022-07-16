4e84eac1cdb2e8b17e3ff7e52eb41285482468ab
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-07-16 22:26:41 +0530

    [pavan] Delete Person Using their Name'

--- AddressBook/AddressBook/AddressBook.cs
+++ AddressBook/AddressBook/AddressBook.cs
@@ -28,7 +28,7 @@ namespace Address_Book
             Console.WriteLine("enter city name");
             contact.City = Console.ReadLine();
 
-            Console.WriteLine("enter stste name");
+            Console.WriteLine("enter state name");
             contact.State = Console.ReadLine();
 
             Console.WriteLine("enter zip name");
@@ -61,7 +61,6 @@ namespace Address_Book
             Console.Write("Enter Phone Email: ");
             person.PhNo = Console.ReadLine();
             Console.Write("Enter Address 1: ");
-
             person.Address = Console.ReadLine();
             addressBook.Add(person);
         }
@@ -74,7 +73,7 @@ namespace Address_Book
                 if (contact.FirstName.ToLower() == firstname.ToLower())
                 {
                     Console.WriteLine("enter which information you wnat to change");
-                    Console.WriteLine("enter 1 to edit last name,2 to change the city, 3 To state, 4 to zip,5 to Email,6 to phonenumber");
+                    Console.WriteLine("enter 1 to edit last name,2 to change the city,3 to zip,4 to Email,5 to phonenumber");
                     int option = Convert.ToInt32(Console.ReadLine());
                     switch (option)
                     {
@@ -87,19 +86,15 @@ namespace Address_Book
                             contact.City = Console.ReadLine();
                             break;
                         case 3:
-                            Console.WriteLine("enter the State");
-                            contact.State = Console.ReadLine();
-                            break;
-                        case 4:
                             Console.WriteLine("enter the zip");
                             contact.Zip = Console.ReadLine();
                             break;
-                        case 5:
+                        case 4:
                             Console.WriteLine("enter the Email");
                             contact.Email = Console.ReadLine();
                             break;
-                        case 6:
-                            Console.WriteLine("enter the phonenumber");
+                        case 5:
+                            Console.WriteLine("enter the phone number");
                             contact.PhNo = Console.ReadLine();
                             break;
                     }
@@ -107,19 +102,28 @@ namespace Address_Book
                 }
             }
             Console.WriteLine("no contact with this firstname");
+
+        }
+        public void RemoveContact()
+        {
+            Console.WriteLine("enter the first name you want to remove");
+            string firstname = Console.ReadLine();
+            foreach (Contact contact in addressBook)
+            {
+                if (contact.FirstName.ToLower() == firstname.ToLower())
+                {
+                    addressBook.Remove(contact);
+                    Console.WriteLine("the firstname you have entered is deleted ");
+                    return;
+                }
+            }
+            Console.WriteLine("contact not found");
         }
         public void Display()
         {
             foreach (Contact contact in addressBook)
             {
-                Console.WriteLine(contact.FirstName);
-                Console.WriteLine(contact.LastName);
-                Console.WriteLine(contact.Address);
-                Console.WriteLine(contact.City);
-                Console.WriteLine(contact.State);
-                Console.WriteLine(contact.PhNo);
-                Console.WriteLine(contact.Zip);
-                Console.WriteLine(contact.Email);
+                Console.WriteLine($"Firstname \t{contact.FirstName}\n Lastname \t {contact.LastName}\n PhoneNumber \t{contact.PhNo}\ncity \t{contact.City}\n zip \t{contact.Zip}\n Email \t{contact.Email}\n Adress \t{contact.Address}\n ");
             }
         }
     }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -3,11 +3,14 @@
 
 
 AddressBook address = new AddressBook();
+
+Console.WriteLine("Welcome to Address Book : ");
+
 string command = "";
 
 while (command != "exit")
 {
-    Console.WriteLine("Please enter a command: add , edit ,display, exit: ");
+    Console.WriteLine("Please enter a command: add , edit, remove, display, exit :");
     command = Console.ReadLine().ToLower();
 
     switch (command)
@@ -18,10 +21,13 @@ while (command != "exit")
         case "edit":
             address.EditContact();
             break;
+        case "remove":
+            address.RemoveContact();
+            break;
         case "display":
             address.Display();
             break;
-        case "exit":
+        default:
             break;
     }
 }
